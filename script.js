2129ac491416c3db585ac83b7f04ac8e10dc659d
AntonisDela <antonisdelavogias@icloud.com>
2024-03-13 13:32:56 +0200

    added an interest functionalit

--- script.js
+++ script.js
@@ -90,6 +90,15 @@ const calcDisplaySummary = function (movements) {
     .filter(val => val < 0)
     .reduce((acc, val) => acc + val, 0);
   labelSumOut.textContent = `${Math.abs(outcomes)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
 };
 calcDisplaySummary(account1.movements);
 
