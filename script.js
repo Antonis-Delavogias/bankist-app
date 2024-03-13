575ac88313731428c2d908aba8f5beece7d7b1c6
AntonisDela <antonisdelavogias@icloud.com>
2024-03-13 13:13:30 +0200

    added the eur sign next to the movements in displayMovement function

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ const displayMovements = function (movements) {
     const html = `
   <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov}€</div>
   </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
