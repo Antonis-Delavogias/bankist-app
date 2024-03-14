b21e2f6337630aa03bfaefd927b908c5d918dd2f
AntonisDela <antonisdelavogias@icloud.com>
2024-03-15 01:19:55 +0200

    created an event listener to the btnClose, so the user can delete his account

--- script.js
+++ script.js
@@ -144,6 +144,26 @@ btnLogin.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    //Delete account
+    accounts.splice(index, 1);
+
+    //Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 //Transfer money
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
